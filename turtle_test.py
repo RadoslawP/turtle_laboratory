3024c7b6114bfb042c9070e1e3a9ef46e5b0f238
RadoslawP <radoslaw.pietraszek.r@gmail.com>
2021-04-18 14:59:59 +0200

    first code in turtle module

---
+++ turtle_test.py
@@ -0,0 +1,15 @@
+import turtle
+
+powolniak = turtle.Turtle()
+powolniak.shape('turtle')
+
+powolniak.forward(100)
+powolniak.right(90)
+powolniak.forward(100)
+powolniak.right(90)
+powolniak.forward(100)
+powolniak.right(90)
+powolniak.forward(100)
+powolniak.right(90)
+
+turtle.mainlloop()
